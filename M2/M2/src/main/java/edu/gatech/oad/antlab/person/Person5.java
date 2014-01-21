3014ac429656cc241f519c32cf95ca874d56bcff
Joseph <gatech.owner@gmail.com>
2014-01-21 04:55:32 -0500

    Joseph P5 file and AntMain.java Change

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -9,16 +9,18 @@ package edu.gatech.oad.antlab.person;
  *  @version 1.1
  */
 public class Person5 {
-  /** Holds the persons real name */
-  private String name;
+  	/** Holds the persons real name */
+  	private String name;
+
   	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
-  public Person5(String pname) {
-    name = pname;
-  }
+	  public Person5(String pname) {
+	    name = pname;
+	  }
+	  
   	/**
 	 * This method should take the string
 	 * input and return its characters rotated
@@ -30,8 +32,7 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+		return input.substring(2) + input.substring(0,2);
 	}
 	
 	/**
