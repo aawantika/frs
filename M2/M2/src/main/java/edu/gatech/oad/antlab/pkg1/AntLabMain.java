0995aab486666bcd5cf329179b107f9e49045596
PravTadikonda <pravtadikonda410@yahoo.com>
2014-01-24 03:05:43 -0500

    Update AntLabMain.java

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -53,24 +53,20 @@ import edu.gatech.oad.antlab.pkg2.*;
         String toPrint = 
             ant11.getMessage() + ant12.getMessage(); //+ ant21.getMessage()
       	// + ant22.getMessage() + ant31.getMessage();
-	//Person1 replace P1 with your name
-	//and gburdell1 with your gt id	  
+      	
+	//Person1 name/GT ID  
 	Person1 p1 = new Person1("Prav Tadikonda");
 	toPrint += p1.toString("ptadikonda3");
-	//Person2 replace P2 with your name
-	//and gburdell with your gt id
+	//Person2 name/GT ID
 	Person2 p2 = new Person2("Jason Bires");
 	toPrint += p2.toString("jbires6");
-	//Person3 replace P3 with your name
-	//and gburdell3 with your gt id
+	//Person3 name/GT ID
 	Person3 p3 = new Person3("Shannon Nguyen");
 	toPrint += p3.toString("snguyen47");
-        //Person4 replace P4 with your name
-        //and gburdell4 with your gt id
+        //Person4 name/GT ID
         Person4 p4 = new Person4("Aawantika Sahu");
         toPrint += p4.toString("asahu9");
-        //Person5 replace P4 with your name
-        //and gburdell5 with your gt id
+        //Person5 name/GT ID
         Person5 p5 = new Person5("HyunJong Lee");
         toPrint += p5.toString("hlee652");
 		  
