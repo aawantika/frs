06cb9d91c2d332ad0e01ed3abcbccd5cfb2217d8
PravTadikonda <pravtadikonda410@yahoo.com>
2014-01-24 02:46:22 -0500

    Update Person1.java

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -1,25 +1,27 @@
 package edu.gatech.oad.antlab.person;
-
 /**
- *  A simple class for person 1
- *  returns their name and a
- *  modified string 
- *  
- *  @author Bob
- *  @version 1.1
+ * A simple class for person 1
+ * returns their name and a modified
+ * string
+ * 
+ * @author Bob; Prav Tadikonda
+ * @version 1.2
  */
 public class Person1 {
-  /** Holds the persons real name */
-  private String name;
-  	/**
-	 * The constructor, takes in the persons
-	 * name
-	 * @param pname the person's real name
-	 */
-  public Person1(String pname) {
-    name = pname;
-  }
-  	/**
+	
+	/** Holds the persons real name */
+	private String name;
+	
+	/**
+     	* The constructor, takes in the persons
+     	* name
+     	* @param pname the person's real name
+     	*/
+	public Person1(String pname) {
+		name = pname;
+	}
+	
+	/**
 	 * This method should take the string
 	 * input and return its characters rotated
 	 * 2 positions.
@@ -30,8 +32,9 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+		String firstHalf = input.substring(0, 2);
+		String secondHalf = input.substring(2);
+		return secondHalf + firstHalf;
 	}
 	
 	/**
@@ -43,7 +46,6 @@ public class Person1 {
 	 *         object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+		return name + calc(input);
 	}
-
 }
