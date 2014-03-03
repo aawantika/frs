8c8c3c1417472b2520e002c0740ca6345ab2e886
leecom3025 <leecom3025@users.noreply.github.com>
2014-03-02 21:48:59 -0500

    Update RegisterActivity.java

--- FinancialApplicationProject/FinancialApplication/src/main/src/edu/gatech/financialapplication/RegisterActivity.java
+++ FinancialApplicationProject/FinancialApplication/src/main/src/edu/gatech/financialapplication/RegisterActivity.java
@@ -1,104 +1,51 @@
 package edu.gatech.financialapplication;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class RegisterActivity extends Activity {
-	File accounts;
-	BufferedWriter writer;
-	private ArrayList<String> userArray;
 
     protected void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
-        userArray = new ArrayList<String>();
-
     	setContentView(R.layout.activity_register);
-    	File directory = new File(Environment.getExternalStorageDirectory() + File.separator + "accounts");
-    	if (!directory.exists())
-    		directory.mkdirs();
-	    	
-    	accounts = new File(directory, "accounts.txt");
-    	try {
-    		if(!accounts.createNewFile()) {
-    			accounts.createNewFile();
-    		}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		loadArray();
+    	Log.d("A", WelcomeActivity.db.getUserDetails("tika").getUsername());
     }
-    
-    public void loadArray() {
-		String filePath = Environment.getExternalStorageDirectory() + File.separator + "accounts" + File.separator + "accounts.txt";
-		try {
-			BufferedReader in = new BufferedReader(new FileReader(filePath));
-			String line = "";
-			while((line = in.readLine()) != null) {
-				userArray.add(line);
-			}
-//			boolean done = false;
-//			while (!done) {
-//				userArray.add(in.readLine());
-//				if(userArray.get(userArray.size() - 1) == null) {
-//					userArray.remove(userArray.size()-1);
-//					done = true;
-//				}
-//			}
-			in.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+
     
     public boolean isDuplicate(String username, String password) {
-    	for(String u : userArray){
-    		u = u.split("&")[0];
-    		if (u.equalsIgnoreCase(username))
-    			return true;
-    	}
+    	if (WelcomeActivity.db.getUserDetailsByUsername(username) != null &&
+    			WelcomeActivity.db.getUserDetailsByUsername(username).getUsername().equalsIgnoreCase(username))
+    		return true;
     	return false;
     }
     
     public void onClick(View view) {
     	String username = ((EditText)findViewById(R.id.userText)).getText().toString();
 		String password = ((EditText)findViewById(R.id.passText)).getText().toString();
-		try {
-			if (isDuplicate(username, password)) {
-				new AlertDialog.Builder(this)
-			    .setTitle("Duplicate")
-			    .setMessage("Credential is duplicate!\nPlease check again.")
-			    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-			        public void onClick(DialogInterface dialog, int which) { 
-			        }
-			     })
-			     .show();
-			}else{
-				BufferedWriter writer = new BufferedWriter(new FileWriter(accounts, true));
-				writer.write(username + "&"+password + "\n");
-				writer.close();
-				Intent intent = new Intent(this, WelcomeActivity.class);
-		    	startActivity(intent);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
+		String firstname = ((EditText)findViewById(R.id.firstText)).getText().toString();
+		String lastname = ((EditText)findViewById(R.id.lastText)).getText().toString();
+		String email = ((EditText)findViewById(R.id.emailText)).getText().toString();
+		if (isDuplicate(username, password)) {
+			new AlertDialog.Builder(this)
+		    .setTitle("Duplicate")
+		    .setMessage("Credential is duplicate!\nPlease check again.")
+		    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+		        public void onClick(DialogInterface dialog, int which) { 
+		        }
+		     })
+		     .show();
+		}else{
+			User user = new User(firstname, lastname, username, password, email);
+			WelcomeActivity.db.addUser(user);
+			Intent intent = new Intent(this, WelcomeActivity.class);
+	    	startActivity(intent);
 		}
-    	
     }
 }
