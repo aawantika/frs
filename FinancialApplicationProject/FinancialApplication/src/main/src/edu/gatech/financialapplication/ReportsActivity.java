576fa6e6b3c6ef72529056a78558d8364bff4815
JasonBires <jason.m.bires@gmail.com>
2014-03-30 22:39:51 -0400

    Fixed checkstyle.

--- FinancialApplicationProject/FinancialApplication/src/main/src/edu/gatech/financialapplication/ReportsActivity.java
+++ FinancialApplicationProject/FinancialApplication/src/main/src/edu/gatech/financialapplication/ReportsActivity.java
@@ -4,27 +4,41 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+/**
+ * Allows the user to select different types of reports.
+ * @author Team 15
+ *
+ */
 public class ReportsActivity extends Activity {
-	private String username, accountNumberTemp;
+    /**
+     * The current username.
+     */
+    private String username;
+	/**
+	 * The current account number.
+	 */
+    private String accountNumberTemp;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_reports);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reports);
 
-		accountNumberTemp = getIntent().getStringExtra("accountNumber");
-		username = getIntent().getStringExtra("username");
-	}
-
-	public void onCSRClick(View view) {
-		Intent intent = new Intent(this, ReportDateActivity.class);
-		Bundle bundle = new Bundle();
-		bundle.putString("type", "consumerSpending");
-		bundle.putString("accountNumber", accountNumberTemp);
-		bundle.putString("username", username);
-		intent.putExtras(bundle);
-		startActivity(intent);
-	}
+        accountNumberTemp = getIntent().getStringExtra("accountNumber");
+        username = getIntent().getStringExtra("username");
+    }
+    /**
+     * Opens a date reporter and tells what type of report to generate.
+     * @param view the current view
+     */
+    public void onCSRClick(View view) {
+        Intent intent = new Intent(this, ReportDateActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("type", "consumerSpending");
+        bundle.putString("accountNumber", accountNumberTemp);
+        bundle.putString("username", username);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
 
 }
