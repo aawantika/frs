c21b9ad84aa1d26adabe26a8c79854edd9e7e15a
Aawantika Sahu <aawantika@gmail.com>
2014-04-12 22:45:44 -0400

    minor change for cash flow

--- FinancialApplicationProject/FinancialApplication/src/main/src/edu/gatech/financialapplication/CashFlowActivity.java
+++ FinancialApplicationProject/FinancialApplication/src/main/src/edu/gatech/financialapplication/CashFlowActivity.java
@@ -13,7 +13,7 @@ import android.widget.TextView;
 public class CashFlowActivity extends Activity {
 
 	private DBHelper db;
-	private String username, accountNumberTemp, finalStart, finalEnd;
+	private String username, accountNumber, finalStart, finalEnd;
 
 	private float totalDeposits, totalWithdrawals, netFlow;
 	private List<Transaction> transactionList;
@@ -25,7 +25,7 @@ public class CashFlowActivity extends Activity {
 		db = new DBHelper(this);
 
 		// pull from intent
-		accountNumberTemp = getIntent().getStringExtra("accountNumber");
+		accountNumber = getIntent().getStringExtra("accountNumber");
 		username = getIntent().getStringExtra("username");
 		finalStart = getIntent().getStringExtra("finalStart");
 		finalEnd = getIntent().getStringExtra("finalEnd");
@@ -34,11 +34,11 @@ public class CashFlowActivity extends Activity {
 		String fixStart = DateGrabber.convertStringToDate(finalStart);
 		String fixEnd = DateGrabber.convertStringToDate(finalEnd);
 		dateText.setText(fixStart + " - " + fixEnd);
-			
+
 		calculateTransactionAmounts();
 		inputTransactionAmounts();
 	}
-	
+
 	/**
 	 * Populates a list with transactions that are within date range.
 	 */
@@ -47,24 +47,27 @@ public class CashFlowActivity extends Activity {
 		for (Transaction t : transactionList) {
 			Log.i("MONTH ", t.getDate());
 			String date = t.getDate();
-			if ((finalStart.compareTo(date) <= 0) && (finalEnd.compareTo(date) >= 0)) {
-				Log.i("transaction in cs", t.toString());
-				if (t.getType().equals("deposit")) {
-					totalDeposits += t.getAmount();
-				} else if (t.getType().equals("withdrawal")) {
-					totalWithdrawals += t.getAmount();
-				}	
+			if (accountNumber.equals(t.getAccount())) {
+				if ((finalStart.compareTo(date) <= 0)
+						&& (finalEnd.compareTo(date) >= 0)) {
+					Log.i("transaction in cs", t.toString());
+					if (t.getType().equals("deposit")) {
+						totalDeposits += t.getAmount();
+					} else if (t.getType().equals("withdrawal")) {
+						totalWithdrawals += t.getAmount();
+					}
+				}
 			}
 		}
 	}
-	
+
 	private void inputTransactionAmounts() {
 		TextView deposits = (TextView) findViewById(R.id.depositText);
 		deposits.setText(Float.toString(totalDeposits));
 		TextView withdrawals = (TextView) findViewById(R.id.withdrawalText);
 		withdrawals.setText(Float.toString(totalWithdrawals));
 		TextView netFlowText = (TextView) findViewById(R.id.netFlowText);
-		
+
 		netFlow = totalDeposits - totalWithdrawals;
 		String netFlowString = "";
 		if (netFlow > 0) {
@@ -76,17 +79,18 @@ public class CashFlowActivity extends Activity {
 			netFlowText.setText(netFlowString);
 			netFlowText.setTextColor(Color.RED);
 		}
- 	}
-	
+	}
+
 	/**
 	 * On click to go back to the general menu.
 	 * 
-	 * @param view The view being used.
+	 * @param view
+	 *            The view being used.
 	 */
-	public void onBackClick (View view) {
+	public void onBackClick(View view) {
 		Intent intent = new Intent(this, TransactionActivity.class);
 		Bundle bundle = new Bundle();
-		bundle.putString("accountNumber", accountNumberTemp);
+		bundle.putString("accountNumber", accountNumber);
 		bundle.putString("username", username);
 		intent.putExtras(bundle);
 		startActivity(intent);
