b3d11954205edfcdb447355c34985ddf0c5cda74
Aawantika Sahu <aawantika@gmail.com>
2014-04-13 01:03:50 -0400

    minor edit to loginactivity

--- FinancialApplicationProject/FinancialApplication/src/main/src/edu/gatech/financialapplication/LoginActivity.java
+++ FinancialApplicationProject/FinancialApplication/src/main/src/edu/gatech/financialapplication/LoginActivity.java
@@ -1,7 +1,5 @@
 package edu.gatech.financialapplication;
 
-import java.util.Map;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -9,7 +7,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
