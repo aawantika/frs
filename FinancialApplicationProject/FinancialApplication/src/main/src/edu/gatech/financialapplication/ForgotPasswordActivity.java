f2e44021a3c126063afa22c39c882af20275d69f
Aawantika Sahu <aawantika@gmail.com>
2014-04-23 22:10:59 -0400

    Update ForgotPasswordActivity.java

--- FinancialApplicationProject/FinancialApplication/src/main/src/edu/gatech/financialapplication/ForgotPasswordActivity.java
+++ FinancialApplicationProject/FinancialApplication/src/main/src/edu/gatech/financialapplication/ForgotPasswordActivity.java
@@ -8,8 +8,6 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.View;
